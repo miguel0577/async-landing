3ac4d25e835d431963b393f0e821d057607c1291
Miguel Buitrago <miguelandresbuitragocruz@gmail.com>
2022-09-02 17:04:23 -0500

    Primera version pagina web

---
+++ src/assets/main.js
@@ -0,0 +1,45 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCQQYrNntItVsT6-QQWsBIAA&part=snippet%2Cid&order=date';
+const content = null ||  document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6d9a47135fmsh862586f3c76b7a4p15886ajsn579af5fb2c3a',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlAPI){
+    const response = await fetch(urlAPI, options);
+    const data = await response.json();
+    console.log(response);
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <a href="https://youtube.com/watch?v=${video.id.videoId}"target="_blank">
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+        `).slice(0,4).join('')}
+        `;
+        content.innerHTML = view;
+    }catch (error){
+            console.log(error);
+        }
+    })();
+
+
